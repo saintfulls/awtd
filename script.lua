9ff3eb705be5997c50cc2ce2306442a0581f10fd
saintfulls <122429232+saintfulls@users.noreply.github.com>
2024-10-30 00:45:05 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -29,6 +29,25 @@ local DefaultSettings = {
     auto_join_level = 1
 }
 
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Rayfield Example Window",
+    LoadingTitle = "Rayfield Interface Suite",
+    LoadingSubtitle = "by Sirius",
+    ConfigurationSaving = {
+        Enabled = false,
+        FolderName = "SapphireHub/Anime World Tower Defense/Settings/", -- Create a custom folder for your hub/game
+        FileName = game.Players.LocalPlayer.UserId .. ".json"
+    },
+    Discord = {
+        Enabled = false,
+        Invite = "noinvitelink",
+        RememberJoins = true
+    },
+    KeySystem = false
+})
+
 if not isfolder("SapphireHub") then
     makefolder("SapphireHub")
 end
@@ -48,6 +67,9 @@ local MacroDefaultSettings = {
 }
 
 local JSON
+
+local startTime
+local startTimeOffset
 
 if not pcall(function()
     readfile(SettingsFile)
@@ -235,20 +257,59 @@ function MacroPlayback()
     end
 end
 
+function StartMacroTimer()
+    repeat
+        task.wait()
+    until not game.Players.LocalPlayer.PlayerGui:WaitForChild("InterFace"):WaitForChild("Skip").Visible and
+        game.Players.LocalPlayer.PlayerGui:WaitForChild("InterFace"):WaitForChild("Skip").topic.Text ~= "[Ready]"
+
+    startTime = os.time()
+    startTimeOffset = os.time()
+    timeElapsed()
+end
+
+function GetMoney()
+    return game.Players.LocalPlayer.leaderstats.Cash.Value
+end
+
+function timeElapsed()
+    if startTime == nil then
+        return -10
+    else
+        return (os.time() - startTime) + (startTimeOffset - startTime)
+    end
+end
+
 local game_metatable = getrawmetatable(game)
 local namecall_original = game_metatable.__namecall
 
 setreadonly(game_metatable, false)
 
--- Modify the metatable
+function CFrameToTable(cframe)
+    local components = {cframe:GetComponents()}
+    return components
+end
+
+function TableToCFrame(tbl)
+    return CFrame.new(unpack(tbl))
+end
+
 game_metatable.__namecall = newcclosure(function(self, ...)
     local method = getnamecallmethod()
     local Args = {...}
 
+    local money = GetMoney()
     if Args and (method == "FireServer" or method == "InvokeServer") then
         if JSON.macro_record and not JSON.macro_playback then
             if self.Name == "SpawnUnit" then
-                -- Logic for SpawnUnit
+                table.insert(Macros[JSON.macro_profile], {
+                    [1] = timeElapsed(),
+                    [2] = {
+                        [1] = Args[1],
+                        [2] = CFrameToTable(Args[2])
+                    },
+                    [3] = money
+                })
             elseif self.Name == "UpgradeUnit" then
                 -- Logic for UpgradeUnit
             elseif self.Name == "ChangeUnitModeFunction" then
@@ -258,14 +319,184 @@ game_metatable.__namecall = newcclosure(function(self, ...)
             elseif self.Name == "SkipEvent" then
                 -- Logic for SkipEvent
             end
-            task.spawn(function() Save() end)
+            task.spawn(function()
+                Save()
+            end)
         end
     end
     return namecall_original(self, ...)
 end)
 
-setreadonly(game_metatable, true)
-
 local Player = game:GetService("Players").LocalPlayer
-repeat task.wait() until #Player.Data:GetChildren() > 0
--- jjgeongezubgezbgihegey_geuizegog_e_go_egeheuiuhgeihgeihegiohegihegiezghiuohegzhiegzieguotoezghiheghegoegzheiguohegzhiezghiegheghzeghzihegzehgziezghiuge
+repeat
+    task.wait()
+until #Player.Data:GetChildren() > 0
+
+if not game.Workspace:FindFirstChild("PlayerPortal") then
+    task.spawn(StartMacroTimer)
+end
+
+local Tabs = {
+    Game = Window:CreateTab("Game", 4483362458),
+    Lobby = Window:CreateTab("Lobby", 4483362458),
+    Macro = Window:CreateTab("Macro", 4483362458),
+    Webhook = Window:CreateTab("Webhook", 4483362458),
+    Miscellaneous = Window:CreateTab("Miscellaneous", 4483362458)
+}
+
+local Macro_Main = Macro:CreateSection("Main")
+local Macro_Settings = Macro:CreateSection("Macro Settings")
+
+local profile_list = {}
+for i, _ in pairs(Macros) do
+    table.insert(profile_list, i)
+end
+
+if Macros[JSON.macro_profile] == nil then
+    for _, v in pairs(profile_list) do
+        if v ~= nil then
+            JSON.macro_profile = v
+            break
+        end
+    end
+end
+
+local Macro_list = Macro_Main:CreateDropdown({
+    Name = "Macro List",
+    Options = profile_list,
+    CurrentOption = {JSON.macro_profile},
+    MultipleOptions = false,
+    Flag = "Dropdown1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Option)
+        JSON.macro_profile = Option
+        if Macros[Option] == nil then
+            Macros[Option] = {}
+        end
+        Save()
+
+        Rayfield:Notify({
+            Title = "Macro Profile",
+            Content = "Using " .. Option,
+            Duration = 6.5,
+            Image = 4483362458,
+            Actions = { -- Notification Buttons
+
+                Ignore = { -- Duplicate this table (or remove it) to add and remove buttons to the notification.
+                    Name = "Okay!",
+                    Callback = function()
+
+                    end
+                }
+
+            }
+        })
+    end
+})
+
+local Macro_Record = Macro_Main:CreateToggle({
+    Name = "Record Macro",
+    CurrentValue = JSON.macro_record,
+    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        JSON.macro_record = Value
+        Save()
+        if not value then
+            Rayfield:Notify({
+                Title = "Macro",
+                Content = "Saved Macro :" .. Option,
+                Duration = 6.5,
+                Image = 4483362458,
+                Actions = { -- Notification Buttons
+
+                    Ignore = { -- Duplicate this table (or remove it) to add and remove buttons to the notification.
+                        Name = "Okay!",
+                        Callback = function()
+
+                        end
+                    }
+
+                }
+            })
+        else
+            Rayfield:Notify({
+                Title = "Macro",
+                Content = "Recording Macro :" .. Option,
+                Duration = 6.5,
+                Image = 4483362458,
+                Actions = { -- Notification Buttons
+
+                    Ignore = { -- Duplicate this table (or remove it) to add and remove buttons to the notification.
+                        Name = "Okay!",
+                        Callback = function()
+
+                        end
+                    }
+
+                }
+            })
+        end
+    end
+})
+
+local Macro_Playback = Macro_Main:CreateToggle({
+    Name = "Play Macro",
+    CurrentValue = JSON.macro_playback,
+    Flag = "Toggle1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+    Callback = function(Value)
+        JSON.macro_playback = Value
+        Save()
+
+        if value then
+            task.spawn(MacroPlayback)
+        end
+    end
+})
+
+local profile_name = ""
+
+local profile_name_text = Macro_Settings:CreateInput({
+    Name = "Profile Name",
+    PlaceholderText = "Enter Name",
+    RemoveTextAfterFocusLost = false,
+    Callback = function(Text)
+        if Text ~= "" then
+            Text = profile_name
+        end
+    end,
+ })
+
+ local Button = Macro_Settings:CreateButton({
+    Name = "Create Profile",
+    Callback = function()
+        if Macros[profile_name] ~= nil then
+            venyx:Notify("Macro Profile", "Macro already exists: " .. profile_name)
+            Rayfield:Notify({
+                Title = "Macro Profile",
+                Content = "Macro already exists :" .. profile_name,
+                Duration = 6.5,
+                Image = 4483362458,
+                Actions = { -- Notification Buttons
+
+                    Ignore = { -- Duplicate this table (or remove it) to add and remove buttons to the notification.
+                        Name = "Okay!",
+                        Callback = function()
+
+                        end
+                    }
+
+                }
+            })
+        else
+            -- creates new profile if it doesn't exist.
+            Macros[profile_name] = {}
+    
+            -- sets current profile to newly created profile
+            JSON.macro_profile = profile_name
+            Save()
+                -- inserts profile into list of profiles.
+            table.insert(profile_list, JSON.macro_profile)
+
+            Macro_list:Set(profile_list)
+        end
+    end,
+ }) 
